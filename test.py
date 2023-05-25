a6a6abc92afd42be3eff01b803536b38219bdc4a
mamasackboy <99284014+mamasackboy@users.noreply.github.com>
2023-05-26 05:57:22 +1000

    Add files via upload

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
 import pygame
+from pygame.locals import *
 import math
 from sheet import *
 from pygame_functions import *
 
 screen_width = 1200
-screen_height = 800
+screen_height = 540
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Gun Chaos")
-background_image = pygame.image.load('Images/MB.jpg')
-background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+background_image = pygame.image.load('Images/gg.png')
 BLACK=(0,0,0)
 
 the_sheet = Spritesheet('Images/Sugnoma.png')
+player1_posx=500.0
+player1_posy=0
+player2_posx=1000.0
+player2_posy=0
 the_rev_sheet=Spritesheet('Images/Sugnoma_reverse.png')
 first_player = the_sheet.get_sprite(0, 0, 128, 125)
 second_player = the_sheet.get_sprite(128, 0, 128, 125)
+
+
 
 left1_animations=[the_rev_sheet.get_sprite(896,128,128,128),
                  the_rev_sheet.get_sprite(896,256,128,128),
@@ -28,6 +34,8 @@ right1_animations=[the_sheet.get_sprite(0,128,128,110),
                   the_sheet.get_sprite(0,384,128,110),
                   the_sheet.get_sprite(0,512,128,110)]
 
+second_player_direction=''
+
 left2_animations=[the_rev_sheet.get_sprite(768,128,128,128),
                  the_rev_sheet.get_sprite(768,256,128,128),
                  the_rev_sheet.get_sprite(768,384,128,128),
@@ -37,27 +45,63 @@ right2_animations=[the_sheet.get_sprite(128,128,128,110),
                   the_sheet.get_sprite(128,256,128,110),
                   the_sheet.get_sprite(128,384,128,110),
                   the_sheet.get_sprite(128,512,128,110)]
+second_player_direction=''
 
-anim_loop=1
-first_player_rect = first_player.get_rect(topleft=(0, screen_height - 500))
-second_player_rect = second_player.get_rect(topleft=(500, screen_height - 500))
 
-gravity = 0.2
+anim_loop=1.0
+first_player_rect = first_player.get_rect(topleft=(500, screen_height - 500))
+second_player_rect = second_player.get_rect(topleft=(1000, screen_height - 500))
+platf_rects = [Rect(1, 420, 850, 150),
+         Rect(170,400, 565, 150),
+        Rect(220,370, 465, 150),
+        Rect(290,340, 320, 150),
+        Rect(340,300, 200, 150),
+        Rect(30,150, 70, 50),
+        Rect(250,150, 280, 50),
+        Rect(800,150, 320, 50),
+        Rect(1100, 420, 850, 150)]
+
+gravity = 0.5
 first_player_vel_y = 0.0
 second_player_vel_y = 0.0
 
+on_platf1_player1 = False
+on_platf1_player2 = False
 
 while True:
-    
+    collide1 = any(first_player_rect.colliderect(platf_rect) for platf_rect in platf_rects)
+    collide2 = any(second_player_rect.colliderect(platf_rect) for platf_rect in platf_rects)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
     keys = pygame.key.get_pressed()
+
+    for platf_rect in platf_rects:
+        if collide1:
+            first_player_rect.bottom = platf_rect.top
+
+        if collide2:
+            second_player_rect.bottom = platf_rect.top
+
+        if first_player_rect.bottom == platf_rect.top and first_player_vel_y >= 0:
+            on_platf1_player1 = True
+        else:
+            on_platf1_player1 = False
+
+        if second_player_rect.bottom == platf_rect.top and second_player_vel_y >= 0:
+            on_platf1_player2 = True
+        else:
+            on_platf1_player2 = False
+
+    
 
     # Move first player with arrow keys
     if keys[pygame.K_LEFT] :
-        proposed_first_player_rect = first_player_rect.move(-2, 0)
+        first_player_direction="G"
+        proposed_first_player_rect = first_player_rect.move(-2*gravity, 0)
+        player1_posx=player1_posx-2*gravity
         first_player=left1_animations[math.floor(anim_loop)]
         anim_loop+=0.01
         if anim_loop>=3:
@@ -74,7 +118,9 @@ while True:
                 
 
     if keys[pygame.K_RIGHT]:
-        proposed_first_player_rect = first_player_rect.move(2, 0)
+        first_player_direction="D"
+        proposed_first_player_rect = first_player_rect.move(2*gravity, 0)
+        player1_posx=player1_posx+2*gravity
         first_player=right1_animations[math.floor(anim_loop)]
         anim_loop+=0.01
         if anim_loop>=3:
@@ -91,7 +137,8 @@ while True:
 
     # Jump with up arrow key
     if keys[pygame.K_UP] :
-        first_player_vel_y = -10
+        first_player_vel_y = -5
+        player1_posy-=5
 
     first_player_vel_y += gravity
     proposed_rect = first_player_rect.move(0, int(first_player_vel_y))
@@ -103,7 +150,9 @@ while True:
                 
 
     if keys[pygame.K_q] :
-        proposed_second_player_rect = second_player_rect.move(-2, 0)
+        second_player_direction="G"
+        proposed_second_player_rect = second_player_rect.move(-2*gravity, 0)
+        player2_posx=player2_posx-2*gravity
         second_player=left2_animations[math.floor(anim_loop)]
         anim_loop+=0.01
         if anim_loop>=4:
@@ -119,7 +168,9 @@ while True:
                 
 
     if keys[pygame.K_d]:
-        proposed_second_player_rect = second_player_rect.move(2, 0)
+        second_player_direction="D"
+        proposed_second_player_rect = second_player_rect.move(2*gravity, 0)
+        player2_posx=player2_posx+2*gravity
         second_player=right2_animations[math.floor(anim_loop)]
         anim_loop+=0.01
         if anim_loop>=4:
@@ -138,7 +189,8 @@ while True:
 
     # Jump with W key
     if keys[pygame.K_z] :
-        second_player_vel_y = -10
+        second_player_vel_y = -5
+        player2_posy-=5
 
     second_player_vel_y += gravity
     proposed_rect = second_player_rect.move(0, int(second_player_vel_y))
@@ -146,7 +198,24 @@ while True:
         second_player_rect = proposed_rect
     else:
         second_player_vel_y = 0.0
+    
+    first_player_vel_y += gravity
+    second_player_vel_y += gravity
 
+    proposed_first_player_rect = first_player_rect.move(0, int(first_player_vel_y))
+    if not proposed_first_player_rect.colliderect(second_player_rect):
+        first_player_rect = proposed_first_player_rect
+
+    proposed_second_player_rect = second_player_rect.move(0, int(second_player_vel_y))
+    if not proposed_second_player_rect.colliderect(first_player_rect):
+        second_player_rect = proposed_second_player_rect
+
+    for platf_rect in platf_rects:
+        if not proposed_first_player_rect.colliderect(platf_rect):
+            first_player_rect = proposed_first_player_rect
+
+        if not proposed_second_player_rect.colliderect(platf_rect):
+            second_player_rect = proposed_second_player_rect
 
 
 
@@ -169,10 +238,11 @@ while True:
     if second_player_rect.bottom > screen_height:
         second_player_rect.bottom = screen_height
     
+    first_player=pygame.transform.scale(first_player,(65,65))
+    second_player=pygame.transform.scale(second_player,(65,65))
     
     screen.fill((0, 0, 0))
     screen.blit(background_image,(0,0))
     screen.blit(first_player,first_player_rect )
     screen.blit(second_player, second_player_rect)
-    pygame.display.update()
-            
+    pygame.display.update()        
